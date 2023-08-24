07cb3a4b974350e7d7f9cc1e72f0691b3ddd68c7
alexw653 <106832607+alexw653@users.noreply.github.com>
2023-08-24 00:17:36 -0400

    added .idea and js

---
+++ .idea/script.js
@@ -0,0 +1,5 @@
+function loadDescription() {
+    const description = `Hello! I'm ChatGPT, a language model developed by OpenAI. I'm here to help answer questions, provide information, or just chat.`;
+
+    document.getElementById("profile-description").textContent = description;
+}
