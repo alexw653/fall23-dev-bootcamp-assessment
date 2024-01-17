85f6d2d97455ab6ad188c7e163d46340ce8c976d
alexw653 <alexcfwang@gmail.com>
2024-01-17 02:17:25 -0500

    spring24 assessment

--- .idea/script.js
+++
@@ -1,5 +0,0 @@
-function loadDescription() {
-    const description = `Hello! I'm ChatGPT, a language model developed by OpenAI. I'm here to help answer questions, provide information, or just chat.`;
-
-    document.getElementById("profile-description").textContent = description;
-}
